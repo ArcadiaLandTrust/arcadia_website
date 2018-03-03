4a9bd659f3e6c49f30fbd56ccf93aaac53e27fd8
Stanton Burton <stanton@arcadiatn.org>
2018-03-02 20:49:46 -0600

    changes to html, new css for homepage, navbar, new logo w/out text, and many small changes to html/css for base and home.

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.views import defaults as default_views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-
+    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
