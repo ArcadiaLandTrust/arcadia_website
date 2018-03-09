cf09000cd0777c0fe30d03f8e1bb3f2f61fbb832
Stanton Burton <stanton@arcadiatn.org>
2018-03-09 12:00:50 -0600

    .

--- config/settings/local.py
+++ config/settings/local.py
@@ -54,9 +54,12 @@ INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
 import socket
 import os
 # tricks to have debug toolbar when developing with docker
-if os.environ.get('USE_DOCKER') == 'yes':
-    ip = socket.gethostbyname(socket.gethostname())
-    INTERNAL_IPS += [ip[:-1] + '1']
+# if os.environ.get('USE_DOCKER') == 'yes':
+#     ip = socket.gethostbyname(socket.gethostname())
+#     INTERNAL_IPS += [ip[:-1] + '1']
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
